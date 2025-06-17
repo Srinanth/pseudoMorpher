3bfbb08a74eb13d3e0ee868eb315bf9175b825cb
touya <touya@kali.smv.com>
2025-06-17 13:11:33 +0530

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin:'http://localhost:5173',credentials:true}));
+app.use(cors({origin:'https://pseudomorph.onrender.com',credentials:true}));
 app.use(express.json());
 
 app.use("/api/gemini", geminiRoutes);
